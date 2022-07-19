46b5cd694c57d5ab5bebb2a93111fd0a56a7a6c1
i.kłusek <J6ZkqwVUn>
2022-07-19 21:58:32 +0200

    hide state data from ReactDevTools, JWT decode

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,13 +1,19 @@
 import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
+import jwt_decode from 'jwt-decode';
 
 const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
+
+    const decoded = auth?.accessToken ? jwt_decode(auth?.accessToken) : undefined;
+
+    const roles = decoded?.UserInfo?.roles || [];
+
     //outlet allows to render all child components, state allows to come back to prev. page
     return (
         <>
-            {auth?.roles?.find((role) => allowedRoles?.includes(role)) ? (
+            {roles?.find((role) => allowedRoles?.includes(role)) ? (
                 <Outlet />
             ) : auth?.user ? (
                 <Navigate to="/unauthorized" state={{ from: location }} replace />
